e8d7f3bdc1f0caa098f97e264700d0b8f7a0d0c2
arsene2803 <kartikshet@gmail.com>
2016-10-23 21:46:42 -0400

    php script updated

--- csvUpdate.php
+++ csvUpdate.php
@@ -1,21 +1,10 @@
 
 <?php
-    $participantID=$_GET['ID'];
-    $vis=$_GET['vis'];
-    $reportedError=$_GET['reportedError'];
-    $truePercent=$_GET['truePercent'];
-    $error=$_GET['error'];
 
-    $file_handle =  fopen("master.csv", "r+") or die("Unable to open file!");
-
-    $row = 0;
-    while (($data = fgetcsv($file_handle, 1000, ",")) !== FALSE) {
-        $row++;
-    }
-
-    $list = array ($participantID,$row,$vis,$reportedError,$truePercent,$error);
-    fputcsv($file_handle, $line); 
+    $file = $_POST['file'];
+    $participant_ID=$_POST['ID'];
+    $file_handle =  fopen($participant_ID+".csv", "w") or die("Unable to open file!");
+    $fwrite($file_handle,$file )
     fclose($file_handle);
-    
-   
+
 ?>
