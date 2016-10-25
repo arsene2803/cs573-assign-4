0f5c82ca0b32f8e6653785561609b3e440c54db5
arsene2803 <kartikshet@gmail.com>
2016-10-25 09:00:57 -0400

    few changes

--- csvUpdate.php
+++ csvUpdate.php
@@ -4,7 +4,7 @@
     $file = $_POST['file'];
     $participant_ID=$_POST['ID'];
     $file_handle =  fopen("n_".$participant_ID.".csv", "w") or die("Unable to open file!");
-    fwrite($file_handle,$file )
+    fwrite($file_handle,$file );
     fclose($file_handle);
 
 ?>
